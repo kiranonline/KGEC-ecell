24c03400124c827bcbaf147b6434d050a823bd30
Deepsubhra Sarkar <deepsubhrasarkar@outlook.com>
2018-03-13 10:04:29 +0530

    carousel sorted

--- js/js_index.js
+++ js/js_index.js
@@ -1,24 +1,30 @@
 $(function () {
     $('.carousel').carousel();
+    $('#demo').on('slide.bs.carousel', function (e) {
+      $(".cc").slideUp(100);
+    });
     $('#demo').on('slid.bs.carousel', function (e) {
-        console.log(e.to);
-        switch(e.to)
-        {
-            case 0:
-                $("#ch1").animate(function(){
-                    "opacity","0"
-                },900);
+        // console.log(e.to);
+        // switch(e.to)
+        // {
+        //     case 0:
+        //         $("#ch1").animate(function(){
+        //             "opacity","0"
+        //         },900);
 
-                break;
-            case 1:
-                $("#ch2").animate(800);
-                console.log("b");
-                break;
-            case 3:
-                $("#ch3").animate(800);
-                break;
+        //         break;
+        //     case 1:
+        //         $("#ch2").animate(800);
+        //         console.log("b");
+        //         break;
+        //     case 3:
+        //         $("#ch3").animate(800);
+        //         break;
+      $(".cc").slideDown(700);
+        
         }
-        });
+        );
+
         // jQuery("#gallery").unitegallery({
         //     gallery_theme: "tiles",
         //      tiles_type: "nested"						
